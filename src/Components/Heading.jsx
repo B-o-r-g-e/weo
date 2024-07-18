d898d9e39279874507d0bc32419c2a3efb9ea161
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-07-18 18:36:33 +0100

    .

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -1,7 +1,7 @@
 const Heading = () => {
     return (
         <h1
-            className={' flex flex-col font-black w-56 md:w-2/3 lg:text-8xl mt-11 lg:mt-5 bg-gradient-to-r from-white via-zinc-700 to-zinc-100 text-transparent bg-clip-text'}>
+            className={' flex flex-col font-black w-56 md:w-2/3 md:text-7xl md:gap-4 lg:text-8xl mt-11 lg:mt-5 bg-gradient-to-r from-white via-zinc-700 to-zinc-100 text-transparent bg-clip-text'}>
              Web3 Design
             <span className={'bg-gradient-to-r from-rose-500 via-rose-400 to-orange-400 inline-block text-transparent bg-clip-text'}>
                 Agency.
