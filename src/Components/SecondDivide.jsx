40d2fd0d2df607f1eb9e27272f8cf78c714d9805
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-07-18 18:39:34 +0100

    .

--- src/Components/SecondDivide.jsx
+++ src/Components/SecondDivide.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const SecondDivide = () => {
     return (
-        <div className={'flex gap-8 lg:justify-between lg:gap-0 lg:px-56 flex-wrap justify-center bg-white bg-opacity-10 backdrop-blur-lg border border-white border-opacity-20 border-x-0 py-5 px-10'}>
+        <div className={'flex gap-8 md:justify-between md:gap-0 lg:px-56 flex-wrap justify-center bg-white bg-opacity-10 backdrop-blur-lg border border-white border-opacity-20 border-x-0 py-5 px-16'}>
             <Achievements count={'5M+'} main={'Customer'} />
             <Achievements count={'450M+'} main={'Coverage'} />
             <Achievements count={'22%'} main={'Earning'} />
